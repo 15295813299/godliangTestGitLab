6b4a9dd8f9422bcbaefb3c9fb27f59873e678fb2
15295813299 <1262371388@qq.com>
2020-11-14 23:13:56 +0800

    222

---
+++ serviceMast/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ClassName:Test
+ * Function:
+ * Reason:
+ *
+ * @Author: zhifeng  Tang
+ * @version:
+ * @Date: Create in 下午 11:13 2020/11/14 0014
+ */
+@Component
+public class Test {
+
+}
