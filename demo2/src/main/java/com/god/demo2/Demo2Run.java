fd2126932c103a97ecfae602828f34364b330d67
15295813299 <1713420303@qq.com>
2020-11-28 15:09:38 +0800

    新增demo

---
+++ demo2/src/main/java/com/god/demo2/Demo2Run.java
@@ -0,0 +1,17 @@
+package com.god.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author GodLiang
+ * @version 1.0
+ * @date 2020/11/28 14:25
+ * @description： TODO
+ */
+@SpringBootApplication
+public class Demo2Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Run.class,args);
+    }
+}
